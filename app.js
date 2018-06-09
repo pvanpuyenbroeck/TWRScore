326bfb1987366ca7968dd9d330eddb79a9b96021
Pieter Van Puyenbroeck <pietervp283@gmail.com>
2018-06-09 19:18:09 +0200

    register and login are working!

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 var port = 8080;
-var serviceAccount = require('./public/mvcscore-firebase-adminsdk-koyk8-a6adcdbe31.json');
-
-firebase.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://mvcscore.firebaseio.com"
-});
+  var config = {
+    apiKey: "AIzaSyBozHGXL0oCEHsaK3KxN1nEHsYoydZvL8g",
+    authDomain: "mvcscore.firebaseapp.com",
+    databaseURL: "https://mvcscore.firebaseio.com",
+    projectId: "mvcscore",
+    storageBucket: "mvcscore.appspot.com",
+    messagingSenderId: "87462869957"
+  };
+  firebase.initializeApp(config);
 
 app.set('view engine', 'ejs');
 
@@ -23,16 +26,28 @@ app.get("/", function(req,res){
 app.get("/login", function(req,res){
     res.render("Login");
 })
+app.get("/register", function(req,res){
+    res.render('register');
+})
 
+app.post("/Register", function(req,res){
+    var email = req.body.email;
+    var pasword = req.body.pasword;
+    firebase.auth().createUserWithEmailAndPassword(email,pasword).catch(function(error){
+        var errorcode = error.code;
+        var errorMessage = error.message;
+    });
+    res.render('home');
+});
 app.post("/Login", function(req,res){
     var email = req.body.email;
     var pasword = req.body.pasword;
-    firebase.auth().createUserWithEmailAndPassword(email,pasword, function(err){
-        if(err){
-            console.log("niet gelukt");
-        }
+    firebase.auth().signInWithEmailAndPassword(email,pasword).catch(function(error){
+        var errorcode = error.code;
+        var errorMessage = error.message;
     });
-})
+    res.render('home');
+});
 
 app.listen(port, function(){
     console.log("Server has started");

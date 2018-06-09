348b2cabfe0929b742de116d73591ee32530da74
Pieter Van Puyenbroeck <pietervp283@gmail.com>
2018-06-09 18:19:08 +0200

    create user

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var admin = require('firebase-admin');
+var firebase = require('firebase');
+var bodyParser = require('body-parser');
 
 //var auth = firebase.auth();
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 var port = 8080;
 var serviceAccount = require('./public/mvcscore-firebase-adminsdk-koyk8-a6adcdbe31.json');
 
-admin.initializeApp({
+firebase.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://mvcscore.firebaseio.com"
 });
@@ -17,9 +20,18 @@ app.set('view engine', 'ejs');
 app.get("/", function(req,res){
     res.render("home");
 })
-
 app.get("/login", function(req,res){
     res.render("Login");
+})
+
+app.post("/Login", function(req,res){
+    var email = req.body.email;
+    var pasword = req.body.pasword;
+    firebase.auth().createUserWithEmailAndPassword(email,pasword, function(err){
+        if(err){
+            console.log("niet gelukt");
+        }
+    });
 })
 
 app.listen(port, function(){

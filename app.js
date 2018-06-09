d0688f03a5af65ce58a1def4d6c7487197f0f69f
Pieter Van Puyenbroeck <pietervp283@gmail.com>
2018-06-09 17:58:53 +0200

    Firebase basics

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require("express");
+var admin = require('firebase-admin');
 
+//var auth = firebase.auth();
 var app = express();
+app.use(express.static(__dirname + "/public"));
 var port = 8080;
+var serviceAccount = require('./public/mvcscore-firebase-adminsdk-koyk8-a6adcdbe31.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mvcscore.firebaseio.com"
+});
 
 app.set('view engine', 'ejs');
 

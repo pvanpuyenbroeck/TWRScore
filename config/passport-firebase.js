788eb16dde9bd8288d6c157efbad3b12a060f4ba
Pieter Van Puyenbroeck <pietervp283@gmail.com>
2018-06-19 15:49:28 +0200

    firebase not working

--- config/passport-firebase.js
+++ config/passport-firebase.js
@@ -1,3 +1,10 @@
+ var firebase = require('firebase');
+ 
+ firebase.initializeApp({
+     serviceAccount: '/Users/pieter/Documents/TWRNode/public/serviceAccount.json',
+     databaseURL: "https://mvcscore.firebaseio.com"
+ });
+
 
 // load all the things we need
 var FirebaseStrategy = require('passport-firebase-auth').Strategy;
@@ -5,15 +12,41 @@ var FirebaseStrategy = require('passport-firebase-auth').Strategy;
 // load up the user model
 var User            = require('../app/models/user');
 
+// expose this function to our app using module.exports
+module.exports = function(passport) {
+    // =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
 
-passport.use(new FirebaseStrategy({
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+        // =========================================================================
+    // LOCAL SIGNUP ============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
+
+  passport.use('local-login', new FirebaseStrategy({
     firebaseProjectId: "mvcscore",
     authorizationURL: 'https://accounts.google.com/o/oauth2/auth',
-    callbackURL: 'https://www.example.net/auth/firebase/callback'
+    callbackURL: 'https://localhost:8080/auth/calback',
+    client_id: "mvcscore",
   },
-  function(accessToken, refreshToken, decodedToken, cb) {
-    User.findOrCreate(..., function (err, user) {
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ FirebaseId: profile.id }, function (err, user) {
       return cb(err, user);
     });
-  }
-));
+  }));
+};

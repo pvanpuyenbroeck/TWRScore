34a4eb6264dcccc5bcf7a8fde9b5f69682c6a6b3
Pieter Van Puyenbroeck <pietervp283@gmail.com>
2018-06-30 16:46:20 +0200

    Team overview maken op home pagina

--- app/routes.js
+++ app/routes.js
@@ -51,9 +51,18 @@ var Player = require("./models/player.js");
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/home', isLoggedIn, function(req, res) {
-        res.render('home.ejs', {
-            user : req.user // get the user out of session and pass to template
+        Team.find({}, function(err, foundTeams){
+            if(err){
+                console.log(err);
+            }else{
+                console.log(foundTeams);
+            res.render('home.ejs', {
+            user : req.user, teams : foundTeams// get the user out of session and pass to template
         });
+            }
+
+     });
+
     });
 
     // =====================================
